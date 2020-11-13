000f2115ae3e28ce270f9f0e997022fc6a0b8f97
Sudhanshu <perfectsudh@gmail.com>
2020-11-13 17:50:32 +0530

    Init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <div className="py-20">
+        <h1 className="text-5xl text-center text-accent-1">
+          Hello World!!
+        </h1>
+      </div>
+    </div>
+  )
+}
